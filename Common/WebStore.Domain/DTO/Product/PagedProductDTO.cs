640a8f92188783c37d944f0a226d76c910f2a353
Ovs <ngovsyannikov@gmail.com>
2019-10-11 10:23:34 +0300

    JS

---
+++ Common/WebStore.Domain/DTO/Product/PagedProductDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.ViewModels;
+
+namespace WebStore.Domain.DTO.Product
+{
+    public class PagedProductDTO
+    {
+        public IEnumerable<ProductDTO> Products { get; set; }
+
+        public int TotalCount { get; set; }
+
+        public object Select(Func<object, ProductViewModel> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
